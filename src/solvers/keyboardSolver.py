fa4cf20990fe4c1cb2a144142c7a49f36d8f55b3
NathanFortyTwo <nathan.feret@gmail.com>
2025-01-12 00:14:36 +0100

    cleanup keayboard solver

--- src/solvers/keyboardSolver.py
+++ src/solvers/keyboardSolver.py
@@ -1,24 +1,12 @@
-from enum import Enum
 from solvers.baseSolver import BaseSolver
-from constants import Symbols as S
-
-# Moche mais améliore la lisiblité du code
-
-VALID_SEQUENCES = [
-    [S.MIRROR, S.AT, S.LAMBDA, S.VOLDEMORT, S.TRIANGLE, S.H_CURSIVE, S.REVERSED_C_DOT],
-    [S.E_2DOT, S.MIRROR, S.REVERSE_P, S.CL, S.EMPTY_STAR, S.H_CURSIVE, S.QUESTION],
-    [S.CIRCLED_C, S.BALLS, S.CL, S.DOUBLE_K, S.HALF_3, S.LAMBDA, S.EMPTY_STAR],
-    [S.SIX, S.REVERSE_P, S.B, S.TRIANGLE, S.DOUBLE_K, S.QUESTION, S.SMILEY],
-    [S.MAJ_PSI, S.SMILEY, S.B, S.C_DOT, S.REVERSE_P, S.HORNED_3, S.FULL_STAR],
-    [S.SIX, S.E_2DOT, S.HASHTAG, S.AE, S.MAJ_PSI, S.H_BAR, S.OMEGA],
-]
+from constants import VALID_SEQUENCES
 
 
 def is_in(candidate, symbol):
     return symbol in candidate
 
 
-class SimpleWire(BaseSolver):
+class KeyBoard(BaseSolver):
     """returns the INDEX of the wire to cut, starting at 0"""
 
     def __init__(self, config):
@@ -45,8 +33,3 @@ class SimpleWire(BaseSolver):
 
         self.input.sort(key=self.key)
         return self.input
-
-
-inputs = [S.MAJ_PSI, S.H_BAR, S.HASHTAG, S.SIX]
-config = {"input": inputs, "bomb_config": 0}
-SimpleWire(config).solve()
