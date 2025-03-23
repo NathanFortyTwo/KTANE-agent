cae6af76690fc0d9621781ad2545ee9b2140fbbf
NathanFortyTwo <nathan.feret@gmail.com>
2025-03-23 19:24:49 +0100

    update buttonsolver

--- src/solvers/button_solver.py
+++ src/solvers/button_solver.py
@@ -1,5 +1,10 @@
 from solvers.base_solver import BaseSolver
-from constants import Color, InvalidInputException, FRButtonText, ButtonActions
+from constants import (
+    Color,
+    InvalidInputException,
+    FRButtonText,
+    ButtonActions,
+)
 
 
 class Button(BaseSolver):
@@ -11,35 +16,41 @@ class Button(BaseSolver):
 
     def validate_input(self):
         super().validate_input()
-        color = self.input.get("color")
-        if type(color) != Color:
+        if type(self.input.get("button_color")) != Color:
+            raise InvalidInputException
+        if type(self.input.get("button_text")) != FRButtonText:
+            raise InvalidInputException
+        if type(self.input.get("button_bandcolor")) != Color:
             raise InvalidInputException
 
     def solve(self):
-        color, text, bandcolor = self.input
-        battery_number = self.config["battery_number"]
-        car = self.config["CAR"]
-        frk = self.config["FRK"]
+        color = self.input.get("button_color")
+        text = self.input.get("button_text")
+        bandcolor = self.input.get("button_bandcolor")
+
+        batteries = self.config["batteries"]
+        car = self.config.get("CAR", None)
+        frk = self.config.get("FRK", None)
 
         # if button not pressed yet
         if bandcolor != Color.BLACK:
             match bandcolor:
                 case Color.BLUE:
-                    return {"Action": "RELEASE", "digit": 4}
+                    return {"Action": ButtonActions.RELEASE, "digit": 4}
                 case Color.WHITE:
-                    return {"Action": "RELEASE", "digit": 1}
+                    return {"Action": ButtonActions.RELEASE, "digit": 1}
                 case Color.YELLOW:
-                    return {"Action": "RELEASE", "digit": 5}
+                    return {"Action": ButtonActions.RELEASE, "digit": 5}
                 case _:
-                    return {"Action": "RELEASE", "digit": 1}
-
+                    return {"Action": ButtonActions.RELEASE, "digit": 1}
+        print(text, color, car)
         if text == FRButtonText.CANCEL and color == Color.BLUE:
             return {"Action": ButtonActions.MAINTAIN}
-        if text == FRButtonText.EXPLODE and battery_number > 1:
+        if text == FRButtonText.EXPLODE and batteries > 1:
             return {"Action": ButtonActions.QUICKTAP}
         if color == Color.WHITE and car:
             return {"Action": ButtonActions.MAINTAIN}
-        if frk and battery_number > 2:
+        if frk and batteries > 2:
             return {"Action": ButtonActions.QUICKTAP}
         if color == Color.YELLOW:
             return {"Action": ButtonActions.MAINTAIN}
