bd5ceb05063ccfc71086394f32765156efe550c0
NathanFortyTwo <nathan.feret@gmail.com>
2025-04-21 13:03:31 +0200

    skip compass until solver is implemented

--- tests/test_compass.py
+++ tests/test_compass.py
@@ -3,9 +3,10 @@ from solvers.base_solver import DEFAULT_BOMB_CONFIG
 import pytest
 
 
+@pytest.mark.skip("Compass solver not implemented yet")
 def test_compass():
-    puzzle_input = [True, True, True, True, True, True, 
-                    True, True, True, True, True, True]
+    puzzle_input = (True, True, True, True, True, True, 
+                    True, True, True, True, True, True)
     bomb_data = DEFAULT_BOMB_CONFIG
     bomb_data["series_number"] = "ABCDE1"
     bomb_data["batteries"] = 7
